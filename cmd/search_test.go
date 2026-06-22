4db6536bcc77db33d85733eeb697340c00bfabbc
binary_soup <contact@binarysoup.dev>
2026-06-22 14:04:08 -0400

    Add tests for search command

---
+++ cmd/search_test.go
@@ -0,0 +1,74 @@
+package cmd_test
+
+import (
+	"pvault/cmd"
+	"pvault/config"
+	"pvault/vault"
+	"testing"
+
+	"github.com/binarysoupdev/go-commando/test"
+	"github.com/binarysoupdev/tinsel/file"
+	"github.com/binarysoupdev/tinsel/pipe"
+	"github.com/binarysoupdev/tinsel/rand"
+	"github.com/stretchr/testify/suite"
+)
+
+type SearchTestSuite struct {
+	test.CommandSuite[*cmd.SearchCommand]
+}
+
+func TestSearchCommandSuite(t *testing.T) {
+	suite.Run(t, &SearchTestSuite{
+		CommandSuite: test.NewCommandSuite(cmd.NewSearchCommand()),
+	})
+}
+
+func (s *SearchTestSuite) SetupTest() {
+	config.SetGlobal(config.Config{
+		VaultPath: file.NewPath(s.T(), "vault"),
+	})
+
+	err := vault.InitializeNew(config.Global.VaultPath)
+	s.Require().NoError(err)
+}
+
+//=====================================
+
+func (s *SearchTestSuite) TestRunInvalidVaultPath() {
+	//-- arrange
+	rand := rand.New(0)
+	NAME := rand.ASCII(15)
+
+	config.Global.VaultPath = "invalid"
+
+	//-- act
+	s.RunCommand("-s", NAME)
+
+	//-- assert
+	s.RequireResultFail("error opening vault")
+}
+
+func (s *SearchTestSuite) TestRunValidDisplaySearchResults() {
+	//-- arrange
+	rand := rand.New(0)
+	NAME := rand.ASCII(15)
+
+	v, err := vault.Open(config.Global.VaultPath)
+	s.Require().NoError(err)
+
+	err = v.SaveRecord(vault.EmptyRecord(NAME))
+	s.Require().NoError(err)
+
+	out := pipe.OpenStdout(1)
+	defer out.Close()
+
+	//-- act
+	s.RunCommand("-s", NAME)
+
+	//-- assert
+	s.RequireResultPass()
+
+	line := out.ReadLine()
+	s.Assert().Contains(line, "[1]")
+	s.Assert().Contains(line, NAME)
+}
