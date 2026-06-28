cc8a406d4eb26ce5ac6bb1420e4abb1a08c6e1a7
binary_soup <contact@binarysoup.dev>
2026-06-28 18:11:36 -0400

    Reduce the number of vault source files

--- vault/crud.go
+++ vault/crud.go
@@ -1,10 +1,13 @@
 package vault
 
 import (
+	"encoding/json"
 	"os"
 	"pvault/errors"
+	"pvault/vault/data"
 	"pvault/vault/record"
 
+	"github.com/binarysoupdev/cryptool/crypt"
 	"github.com/google/uuid"
 )
 
@@ -27,6 +30,39 @@ func (v Vault) SaveRecord(r record.Record, password string) error {
 	return nil
 }
 
+func (v Vault) saveEncryptedRecord(r record.Record, password string) error {
+	c, salt := crypt.NewFromPassword(password)
+
+	plaintext, err := json.Marshal(r)
+	if err != nil {
+		return errors.Chain(err, "error marshaling json")
+	}
+
+	ciphertext := c.Encrypt(plaintext)
+
+	err = os.WriteFile(v.RecordPath(r.ID), append(salt, ciphertext...), 0666)
+	if err != nil {
+		return errors.Chain(err, "error writing record file")
+	}
+
+	return nil
+}
+
+func (v Vault) updateIndex(r record.Record) error {
+	existingName, ok := v.Index.FindName(r.ID)
+	if ok && existingName != r.Name {
+		delete(v.Index, existingName)
+	}
+	v.Index[r.Name] = r.ID
+
+	err := data.CurrentDatabase{}.SaveIndex(v.IndexPath(), v.Index)
+	if err != nil {
+		return errors.Chain(err, "error saving index file")
+	}
+
+	return nil
+}
+
 func (v Vault) LoadRecord(name string, password string) (record.Record, error) {
 	id, ok := v.Index[name]
 	if !ok {
@@ -36,6 +72,29 @@ func (v Vault) LoadRecord(name string, password string) (record.Record, error) {
 	r, err := v.loadEncryptedRecord(id, password)
 	if err != nil {
 		return record.Record{}, err
+	}
+
+	return r, nil
+}
+
+func (v Vault) loadEncryptedRecord(id uuid.UUID, password string) (record.Record, error) {
+	r := record.Record{}
+
+	raw, err := os.ReadFile(v.RecordPath(id))
+	if err != nil {
+		return r, errors.Chain(err, "error reading record file")
+	}
+
+	c := crypt.LoadFromPassword(password, raw[:crypt.SALT_SIZE])
+
+	plaintext, err := c.Decrypt(raw[crypt.SALT_SIZE:])
+	if err != nil {
+		return r, errors.Chain(err, "error decrypting ciphertext")
+	}
+
+	err = json.Unmarshal(plaintext, &r)
+	if err != nil {
+		return r, errors.Chain(err, "error unmarshaling json")
 	}
 
 	return r, nil
@@ -58,4 +117,15 @@ func (v Vault) DeleteRecord(name string) (uuid.UUID, error) {
 	}
 
 	return id, nil
+}
+
+func (v Vault) deleteIndex(name string) error {
+	delete(v.Index, name)
+
+	err := data.CurrentDatabase{}.SaveIndex(v.IndexPath(), v.Index)
+	if err != nil {
+		return errors.Chain(err, "error saving index file")
+	}
+
+	return nil
 }
