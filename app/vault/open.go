d818cfeb55630fa86aaabf9149e85033a7877472
binary_soup <contact@binarysoup.dev>
2026-07-30 18:28:46 -0400

    Add open tests

--- app/vault/open.go
+++ app/vault/open.go
@@ -4,9 +4,9 @@ import (
 	"os"
 	"path/filepath"
 	"pvault/app/vault/database"
-	v1 "pvault/app/vault/database/database/v1"
-	v2 "pvault/app/vault/database/database/v2"
-	v3 "pvault/app/vault/database/database/v3"
+	db_v1 "pvault/app/vault/database/database/v1"
+	db_v2 "pvault/app/vault/database/database/v2"
+	db_v3 "pvault/app/vault/database/database/v3"
 	"pvault/app/vault/meta"
 
 	"github.com/binarysoupdev/go-commando/errors"
@@ -51,27 +51,27 @@ func createOrLoadMetadata(v *Vault) error {
 }
 
 func detectLegacyDatabase(path string) (int, bool) {
-	_, err := os.Stat(filepath.Join(path, v2.INDEX_FILE))
+	_, err := os.Stat(filepath.Join(path, db_v2.INDEX_FILE))
 	if err == nil {
-		return v2.VERSION, true
+		return db_v2.VERSION, true
 	}
 
-	_, err = os.Stat(filepath.Join(path, v1.INDEX_FILE))
+	_, err = os.Stat(filepath.Join(path, db_v1.INDEX_FILE))
 	if err == nil {
-		return v1.VERSION, true
+		return db_v1.VERSION, true
 	}
 
-	return 0, false
+	return -1, false
 }
 
 func createDatabaseFromVersion(version int) (database.Database, error) {
 	switch version {
-	case v1.VERSION:
-		return v1.Database{}, nil
-	case v2.VERSION:
-		return v2.Database{}, nil
-	case v3.VERSION:
-		return v3.Database{}, nil
+	case db_v1.VERSION:
+		return db_v1.Database{}, nil
+	case db_v2.VERSION:
+		return db_v2.Database{}, nil
+	case db_v3.VERSION:
+		return db_v3.Database{}, nil
 	default:
 		return nil, errors.Format("unsupported vault version \"%d\"", version)
 	}
---
+++ app/vault/open_test.go
@@ -0,0 +1,135 @@
+package vault_test
+
+import (
+	"fmt"
+	"path/filepath"
+	"pvault/app/vault"
+	"pvault/app/vault/database"
+	db_v1 "pvault/app/vault/database/database/v1"
+	db_v2 "pvault/app/vault/database/database/v2"
+	db_v3 "pvault/app/vault/database/database/v3"
+	"pvault/app/vault/index"
+	"pvault/app/vault/meta"
+	"testing"
+
+	"github.com/binarysoupdev/tinsel/file"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpenReturnsErrorWhenDatabaseNotFound(t *testing.T) {
+	//-- arrange
+	PATH := file.NewPath(t, "")
+
+	//-- act
+	_, res := vault.Open(PATH)
+
+	//-- assert
+	assert.ErrorContains(t, res, fmt.Sprintf("vault not found at \"%s\"", PATH))
+}
+
+func TestOpenReturnsVaultAndNoErrorWhenDatabaseIsV1AndCreatesNewMetadata(t *testing.T) {
+	//-- arrange
+	PATH := file.NewPath(t, "")
+
+	INDEX := index.IndexMap{
+		"name": uuid.New(),
+	}
+	require.NoError(t, database.SaveIndex(db_v1.Database{}, PATH, INDEX))
+
+	//-- act
+	res, err := vault.Open(PATH)
+
+	//-- assert
+	require.NoError(t, err)
+
+	assert.Equal(t, PATH, res.Path)
+	assert.Equal(t, INDEX, res.Map)
+	assert.IsType(t, db_v1.Database{}, res.Database)
+
+	assert.FileExists(t, res.MetadataPath())
+	assert.Equal(t, db_v1.VERSION, res.Meta.DatabaseVersion)
+}
+
+func TestOpenReturnsVaultAndNoErrorWhenDatabaseIsV2AndCreatesNewMetadata(t *testing.T) {
+	//-- arrange
+	PATH := file.NewPath(t, "")
+
+	INDEX := index.IndexMap{
+		"name": uuid.New(),
+	}
+	require.NoError(t, database.SaveIndex(db_v2.Database{}, PATH, INDEX))
+
+	//-- act
+	res, err := vault.Open(PATH)
+
+	//-- assert
+	require.NoError(t, err)
+
+	assert.Equal(t, PATH, res.Path)
+	assert.Equal(t, INDEX, res.Map)
+	assert.IsType(t, db_v2.Database{}, res.Database)
+
+	assert.FileExists(t, res.MetadataPath())
+	assert.Equal(t, db_v2.VERSION, res.Meta.DatabaseVersion)
+}
+
+func TestOpenReturnsErrorWhenErrorLoadingMetadata(t *testing.T) {
+	//-- arrange
+	PATH := file.CreateEmpty(t, vault.METADATA_FILE)
+
+	//-- act
+	_, res := vault.Open(filepath.Dir(PATH))
+
+	//-- assert
+	assert.ErrorContains(t, res, "error loading metadata")
+}
+
+func TestOpenReturnsVaultAndNoErrorWhenDatabaseIsV3AndLoadsMetadata(t *testing.T) {
+	//-- arrange
+	PATH := file.NewPath(t, "")
+	INDEX := index.IndexMap{
+		"name": uuid.New(),
+	}
+	require.NoError(t, database.SaveIndex(db_v3.Database{}, PATH, INDEX))
+
+	VAULT := vault.Vault{
+		Path: PATH,
+		Meta: meta.Metadata{
+			DatabaseVersion: db_v3.VERSION,
+		},
+	}
+	require.NoError(t, VAULT.SaveMetadata())
+
+	//-- act
+	res, err := vault.Open(PATH)
+
+	//-- assert
+	require.NoError(t, err)
+
+	assert.Equal(t, PATH, res.Path)
+	assert.Equal(t, INDEX, res.Map)
+	assert.IsType(t, db_v3.Database{}, res.Database)
+
+	assert.Equal(t, VAULT.Meta, res.Meta)
+}
+
+func TestOpenReturnsErrorWhenDatabaseVersionUnsupported(t *testing.T) {
+	//-- arrange
+	PATH := file.NewPath(t, "")
+
+	VAULT := vault.Vault{
+		Path: PATH,
+		Meta: meta.Metadata{
+			DatabaseVersion: db_v3.VERSION + 1,
+		},
+	}
+	require.NoError(t, VAULT.SaveMetadata())
+
+	//-- act
+	_, res := vault.Open(PATH)
+
+	//-- assert
+	assert.ErrorContains(t, res, fmt.Sprintf("unsupported vault version \"%d\"", VAULT.Meta.DatabaseVersion))
+}
